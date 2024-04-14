a84bf9ab8270b50b9ff8b9b8a7f6daac45e0ac02
Salty Frappuccino <saltyfrappuccino@internet.ru>
2024-04-14 23:27:48 +0400

    Add `app`

---
+++ app/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import RedirectResponse
+from app.routes.chat import chat_router
+from app.routes.vacancy import vacancy_router
+
+import uvicorn
+
+app = FastAPI()
+app.include_router(vacancy_router)
+app.include_router(chat_router)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Gigachat HR",
+        version="1.0.00",
+        summary="Gigachat HR - API для упрощения работы HR",
+        # description="",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css",
+    )
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ app/routes/chat.py
@@ -0,0 +1,50 @@
+import json
+from typing import Union
+
+import requests
+from fastapi import APIRouter
+from pydantic import BaseModel, Field
+
+from app.utils.token import get_token
+
+chat_router = APIRouter(prefix="/chat")
+
+
+class Message(BaseModel):
+    content: Union[str, None] = Field(default=None, examples=["Привет!"])
+
+
+messages = []
+
+
+@chat_router.post("/")
+async def chat(message: Message):
+    '''
+    Простой запрос к Гигачату
+    '''
+    message = {'role': 'user', 'content': message.content}
+    messages.append(message)
+
+    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+
+    payload = json.dumps({
+        "model": "GigaChat",
+        "messages": messages,
+        "max_tokens": 512,
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + get_token(),
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+    role = response.json()['choices'][0]['message']['role']
+    content = response.json()['choices'][0]['message']['content']
+    message = {'role': role, 'content': content}
+    messages.append(message)
+    return response.json()['choices'][0]['message']['content']
+
+@chat_router.get("/")
+async def get_messages():
+    return messages
